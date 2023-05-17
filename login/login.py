c93eb040dbecee05345d47f0b6817d49d2230a97
0sik <ghdtlrdudtlr@naver.com>
2023-05-18 03:18:19 +0900

    chore : 로그인,회원가입 api url 수정

--- login/login.py
+++ login/login.py
@@ -9,35 +9,35 @@ app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 CORS(app)
 
-login = Blueprint('login/member', __name__)
+login = Blueprint('api/login/member', __name__)
 
-@login.route('/login/member', methods=['GET', 'POST'])
+@login.route('api/login/member', methods=['GET', 'POST'])
 def Login():
-    userName = request.json.get('userName')
     phoneNum = request.json.get('phoneNum')
+    rrNum = request.json.get('rrNum')
 
-    sql = f"SELECT * FROM user WHERE userName='{userName}' AND phoneNum='{phoneNum}'"
+    sql = f"SELECT * FROM user WHERE phoneNum='{phoneNum}' AND rrNum='{rrNum}'"
     conn = db_connect.ConnectDB(sql)
     conn.execute()
     user = conn.fetch()
     del conn
 
     if user:
-        session['logged_in'] = userName
+        session['logged_in'] = phoneNum
         session.permanent = True
         app.permanent_session_lifetime = timedelta(minutes=30)
         return jsonify({'message': 'Login successful!'})
     else:
         return jsonify({'message': 'Invalid credentials!'})
     
-@app.route('/logout', methods=['POST'])
+@app.route('api/logout', methods=['POST'])
 def logout():
-    userName = request.json.get('userName')
+    phoneNum = request.json.get('phoneNum')
     # 로그아웃 요청 처리 코드
     session.pop('logged_in', None) # 세션에서 로그인 정보 삭제
     return jsonify({'message': 'Logout successful!'})
 
-@app.route('/protected', methods=['GET'])
+@app.route('api/protected', methods=['GET'])
 def protected():
     # 인가가 필요한 API 요청 처리 코드
     if not session.get('logged_in'):
