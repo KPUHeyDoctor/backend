f5f22d876110b24f1a82b5e18c1edf3c52288be8
hellojunho <jo62698476@gmail.com>
2023-06-24 23:28:27 +0900

    feat    : reservation/reservation.py 예약 api 구현

--- reservation/reservation.py
+++ reservation/reservation.py
@@ -6,3 +6,43 @@ app = Flask(__name__)
 CORS(app)
 
 reservation = Blueprint('reservation', __name__)
+
+@reservation.route('/api/reservation')
+def Reservation():
+    enterpriseName = request.args.get('enterpriseName')
+    
+    sql = f"SELECT doctorName, doctorMedical FROM doctor\
+        INNER JOIN enterprise ON doctor.enterpkId = enterprise.enterpkId\
+        WHERE enterprise.enterpriseName = '{enterpriseName}';"
+    
+    db = ConnectDB(sql)
+    result = db.fetch()
+    
+    return jsonify(result)
+
+
+@reservation.route('/api/reservation/doctor')
+def Doctor():
+    doctorName = request.args.get('doctorName')
+    sql = f"select * from doctor where doctorName='{doctorName}';"
+
+    conn = ConnectDB(sql)
+    conn.execute()
+    result = conn.fetch()
+    del conn
+
+    return jsonify(result)
+
+@reservation.route('/api/reservation/doctor/detail', methods=['POST'])
+def Detail():
+    data = request.get_json()
+
+    try:
+        usernames = [item['username'] for item in data]
+        username_count = len(usernames)
+        return jsonify({'count':username_count})
+    except:
+        return "Invalid request"
+
+
+app.register_blueprint(reservation, url_prefix='/api/reservation')
